c9b59410e138bd12ebf17528402d4014b9dfd0d5
Kate <e.sheverdenok@gmail.com>
2023-12-26 15:55:21 +0000

    added comparing functionality

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,9 +9,21 @@ import picocli.CommandLine.Parameters;
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+    @Parameters(paramLabel = "filePath1", description = "path to first file")
+    private String filePath1;
+    @Parameters(paramLabel = "filePath2", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format = "stylish";
+
     @Override
     public void run() {
-        System.out.println("file with difference");
+        try {
+            System.out.println(Differ.generate(filePath1, filePath2));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,50 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Differ {
     public static String generate(String filePath1, String filePath2) {
-        return "Hello, world";
+        Map<String, String> json1 = getData(filePath1);
+        Map<String, String> json2 = getData(filePath2);
+
+        List<String> mergedKeys = new ArrayList<>(json1.keySet());
+        for (String key : json2.keySet()) {
+            if (!(json1.containsKey(key))) mergedKeys.add(key);
+        }
+
+        List<String> result = new ArrayList<>();
+        mergedKeys.stream()
+                .sorted()
+                .map(e -> {
+                    if (json1.containsKey(e) && json2.containsKey(e)) {
+                        return json1.get(e).equals(json2.get(e)) ? List.of("  " + e + ": " + json1.get(e)) : List.of("- " + e + ": " + json1.get(e), "+ " + e + ": " + json2.get(e));
+                    } else if (json1.containsKey(e) && !(json2.containsKey(e))) {
+                        return List.of("- " + e + ": " + json1.get(e));
+                    } else {
+                        return List.of("+ " + e + ": " + json2.get(e));
+                    }
+                })
+                .forEach(e -> result.addAll(e));
+
+        return result.toString();
+    }
+
+    private static Map<String, String> getData(String filePath) {
+        Path file = Paths.get(filePath).isAbsolute() ? Paths.get(filePath).normalize() : Paths.get(filePath).toAbsolutePath().normalize();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String content = Files.readString(file).trim();
+            return mapper.readValue(content, new TypeReference<Map<String, String>>() { });
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
